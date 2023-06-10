f1f232960dfee54ae61cbe8cf1af79fe9227d940
Zhorik Zeniuk <ZhorikZeniuk@gmail.ru>
2023-06-10 15:00:13 +0200

    homework1 done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,20 @@
--- SQL-команды для создания таблиц
+CREATE TABLE customers (
+    customer_id varchar(10) PRIMARY KEY NOT NULL,
+    company_name varchar(50),
+    contact_name varchar(50)
+);
+CREATE TABLE employees(
+  emploee_id int PRIMARY KEY NOT NULL,
+  first_name varchar(50),
+  last_name varchar(50),
+  title varchar(150),
+  birth_date date,
+  notes text
+);
+CREATE TABLE orders(
+    order_id int PRIMARY KEY,
+    customer_id  varchar(10) REFERENCES customers(customer_id) NOT NULL,
+    employee_id  int REFERENCES employees(emploee_id) NOT NULL,
+    order_date date,
+    ship_sity varchar(255)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,66 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+conn = psycopg2.connect(host='localhost', port='5433', database='north', user='ZhorikZeniuk', password='cucumber1l')
+
+
+def insert_values_in_table_customers():
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                with open('north_data/customers_data.csv') as file:
+                    content = csv.DictReader(file)
+                    for row in content:
+                        try:
+                            cur.execute('INSERT INTO customers VALUES (%s, %s, %s)',
+                                        (row['customer_id'], row['company_name'], row['contact_name']))
+                        except psycopg2.errors.UniqueViolation:
+                            print('Ошибка транзакции, транзакция отменена.')
+                            conn.rollback()
+                            continue
+    finally:
+        conn.close()
+
+
+def insert_values_in_table_employees():
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                with open('north_data/employees_data.csv') as file:
+                    content = csv.DictReader(file)
+                    for row in content:
+                        try:
+                            cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)',
+                                        (row['employee_id'], row['first_name'], row['last_name'],
+                                         row['title'], row['birth_date'], row['notes']))
+                        except psycopg2.errors.UniqueViolation:
+                            print('Ошибка транзакции, транзакция отменена.')
+                            conn.rollback()
+                            continue
+    finally:
+        conn.close()
+
+
+def insert_values_in_table_orders():
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                with open('north_data/orders_data.csv') as file:
+                    content = csv.DictReader(file)
+                    for row in content:
+                        try:
+                            cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
+                                        (row['order_id'], row['customer_id'], row['employee_id'],
+                                         row['order_date'], row['ship_city']))
+                        except psycopg2.errors.UniqueViolation:
+                            print('Ошибка транзакции, транзакция отменена.')
+                            conn.rollback()
+                            continue
+    finally:
+        conn.close()
+
+
+insert_values_in_table_customers()
+insert_values_in_table_employees()
+insert_values_in_table_orders()
